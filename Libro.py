b83fddc82b45c268aa2e83f9ddfb933ea7f06f10
pato1324 <85973400+pato1324@users.noreply.github.com>
2022-09-08 10:53:54 -0500

    Se agrego la clase libroadmin jsjsjsjs

--- Libro.py
+++ Libro.py
@@ -10,3 +10,29 @@ class Libro(object):
 
     def __getattribute__(self, item):
         return object.__getattribute__(self, item)
+      
+class LibroAdmin(object):
+    def __init__(self):
+        self.arregloLibros = []
+
+    def agregar(self, id, titulo, autor, editorial):
+        lib = Libro(id, titulo, autor, editorial)
+        self.arregloLibros.append(lib)
+
+    def __str__(self):
+        s = ""
+        for libro in self.arregloLibros:
+            s += str(libro) + '\n'
+        return s
+
+    def buscar(self, clave, por="id"):
+        for indice, libro in enumerate(self.arregloLibros):
+            if libro.__getattribute__(por) == clave:
+                return indice
+        return None
+
+    def remover(self, clave, por='id'):
+        indice = self.buscar(clave)
+        if indice is not None:
+            self.arregloLibros.pop(indice)
+        return indice
